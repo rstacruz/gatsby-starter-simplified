005a2f2e43ee2abec9ef41092bc5716c4e6287d1
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2018-11-01 14:01:17 +0800

    Remove prop-types

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
 
@@ -33,9 +32,5 @@ const Layout = ({ children }) => (
     )}
   />
 )
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
 
 export default Layout
