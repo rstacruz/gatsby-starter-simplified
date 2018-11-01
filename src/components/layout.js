e4ebbb42a4ab426e0edda887208b9b7f60c24dd3
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2018-11-01 17:30:26 +0800

    Remove header

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,8 +2,6 @@ import React from 'react'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
 
-import Header from './header'
-
 const Layout = ({ children }) => (
   <StaticQuery
     query={graphql`
