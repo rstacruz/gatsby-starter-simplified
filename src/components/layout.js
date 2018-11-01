f5494b15930b5f712f76cdba8638489e46060234
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2018-11-01 13:59:26 +0800

    Remove layout CSS

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,7 +4,6 @@ import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
 
 import Header from './header'
-import './layout.css'
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -28,17 +27,8 @@ const Layout = ({ children }) => (
         >
           <html lang="en" />
         </Helmet>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
-        >
-          {children}
-        </div>
+        <h1>{data.site.siteMetadata.title}</h1>
+        <div>{children}</div>
       </>
     )}
   />
