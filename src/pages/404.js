4090a1a6121c392e17f497cf69753ad8fc5119e9
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2019-01-31 15:18:00 +0800

    Rework the 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,8 +3,9 @@ import Layout from '../components/Layout'
 
 const NotFoundPage = () => (
   <Layout>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div style={{ maxWidth: '200px', margin: '45vh auto 0 auto', textAlign: 'center' }}>
+    <h1>Oops!</h1>
+    <p>This page doesn't exist yet.</p>
   </Layout>
 )
 
