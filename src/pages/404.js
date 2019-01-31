431e325c2a21df088a8b3abd07a15fc5b365cfa7
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2019-01-31 15:31:38 +0800

    Fix 404 syntax error

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,8 +4,9 @@ import Layout from '../components/Layout'
 const NotFoundPage = () => (
   <Layout>
     <div style={{ maxWidth: '200px', margin: '45vh auto 0 auto', textAlign: 'center' }}>
-    <h1>Oops!</h1>
-    <p>This page doesn't exist yet.</p>
+      <h1>Oops!</h1>
+      <p>This page doesn't exist yet.</p>
+    </div>
   </Layout>
 )
 
