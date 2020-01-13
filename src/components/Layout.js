4712f2b4444f5c3e2bb09e34cdf5f61d8b2df115
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2020-01-13 21:39:47 +1100

    Use hooks for static queries

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,36 +1,40 @@
 import React from 'react'
 import Helmet from 'react-helmet'
-import { StaticQuery, graphql } from 'gatsby'
+import { useStaticQuery, graphql } from 'gatsby'
 
 import 'sanitize.css'
 
-const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
+const useSiteMetadata = () => {
+  const query = graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
         }
       }
-    `}
-    render={data => (
-      <>
-        <Helmet
-          title={data.site.siteMetadata.title}
-          meta={[
-            { name: 'description', content: 'Sample' },
-            { name: 'keywords', content: 'sample, something' },
-          ]}
-        >
-          <html lang="en" />
-        </Helmet>
-        {/*<h1>{data.site.siteMetadata.title}</h1>*/}
-        <div>{children}</div>
-      </>
-    )}
-  />
-)
+    }
+  `
+  return useStaticQuery(query).site.siteMetadata
+}
+
+const Layout = ({ children }) => {
+  const siteMetadata = useSiteMetadata()
+
+  return (
+    <>
+      <Helmet
+        title={siteMetadata.title}
+        meta={[
+          { name: 'description', content: 'Sample' },
+          { name: 'keywords', content: 'sample, something' }
+        ]}
+      >
+        <html lang='en' />
+      </Helmet>
+      {/*<h1>{siteMetadata.title}</h1>*/}
+      <div>{children}</div>
+    </>
+  )
+}
 
 export default Layout
